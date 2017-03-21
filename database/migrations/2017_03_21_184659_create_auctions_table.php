9412d9603a5f815a70496b507e4c0055922eb507
Ismaaa <didouhismael@gmail.com>
2017-03-21 20:41:53 +0100

    Afegit teorema de tales i 5 nous algoritmes

--- database/migrations/2017_03_21_184659_create_auctions_table.php
+++ database/migrations/2017_03_21_184659_create_auctions_table.php
@@ -17,7 +17,7 @@ class CreateAuctionsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('product_id');
+            $table->integer('product_id')->unsigned();
             $table->foreign('product_id')->references('id')->on('products');
             $table->text('description');
             $table->boolean('active');
